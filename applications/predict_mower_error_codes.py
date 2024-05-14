99f272b0e27e8fe0be9eeb80b72223de8ce2063b
Jesper Frisk <61691900+MrDweller@users.noreply.github.com>
2024-05-14 09:07:15 +0200

    adds application for notifying by error codeserror code

---
+++ applications/predict_mower_error_codes.py
@@ -0,0 +1,117 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import DecisionTreeClassificationModel
+from pyspark.ml.feature import StringIndexerModel
+from pyspark.ml.feature import IndexToString
+
+from pyspark.ml.feature import VectorAssembler
+import requests
+import os
+
+import arrowhead.serviceregistry
+import arrowhead.orchestrator
+
+
+CERT_FILE_PATH = os.environ["CERT_FILE_PATH"]
+KEY_FILE_PATH = os.environ["KEY_FILE_PATH"]
+
+ADDRESS=os.environ["DOMAIN_ADDRESS"]
+PORT=os.environ["DOMAIN_PORT"]
+SYSTEM_NAME="anomaly-detector-error-codes"
+SERVICE_REGISTRY_ADDRESS = os.environ["SERVICE_REGISTRY_ADDRESS"]
+SERVICE_REGISTRY_PORT = serviceregistry_port=os.environ["SERVICE_REGISTRY_PORT"]
+SERVICE_REGISTRY_SECURITY_MODE = serviceregistry_security_mode=os.environ["SERVICE_REGISTRY_SECURITY_MODE"]
+
+SERVICE_REGISTRY_CONFIG = serviceregistry_config=arrowhead.serviceregistry.ServiceRegistryConfig(
+    serviceregistry_address=SERVICE_REGISTRY_ADDRESS,
+    serviceregistry_port=SERVICE_REGISTRY_PORT,
+    serviceregistry_security_mode=arrowhead.serviceregistry.security.SecurityMode.from_str(SERVICE_REGISTRY_SECURITY_MODE),
+)
+
+def handle_stream_prediction(df, epoch_id):
+    df.show()
+
+    list_of_predictions = [row.asDict() for row in df.collect()]
+
+    for prediction in list_of_predictions:
+        if prediction["state"] != "normal":
+            state = prediction["state"] 
+            id = prediction["id"]
+            time = prediction["timestamp"]
+            print(f"found error state: {state}, for {id} at time {time}")
+
+            orchestration_response = arrowhead.orchestrator.orchestration(requested_service_definition=state, requester_system_address=ADDRESS, requester_system_port=PORT, requester_system_name=SYSTEM_NAME, serviceregistry_config=SERVICE_REGISTRY_CONFIG, cert=(CERT_FILE_PATH, KEY_FILE_PATH))
+
+            print("\n", orchestration_response)
+
+            for response in orchestration_response["response"]:
+                if (response["provider"]["systemName"] == id):
+                    address = response["provider"]["address"]
+                    port = response["provider"]["port"]
+                    uri = response["serviceUri"]
+
+                    requests.post(f"https://{address}:{port}{uri}", cert=(CERT_FILE_PATH, KEY_FILE_PATH), verify=False)
+
+
+if __name__ == "__main__":
+
+    # Register this anomaly detector in the arrowhead service registry
+    arrowhead.serviceregistry.register_system(
+        address=ADDRESS, 
+        port=PORT, 
+        system_name=SYSTEM_NAME, 
+        serviceregistry_config=SERVICE_REGISTRY_CONFIG, 
+        cert=(CERT_FILE_PATH, KEY_FILE_PATH)
+    )
+
+    stream_directory =  "data/stream/mower" # stream data from a local directory 
+    if not os.path.exists(stream_directory):
+        os.makedirs(stream_directory)
+
+    spark = SparkSession.builder\
+        .appName("Predict mower error codes")\
+        .master("spark://spark-master:7077")\
+        .getOrCreate()
+    sc = spark.sparkContext
+    sc.setLogLevel("WARN")
+
+                                  
+    streamingDF = spark.readStream\
+        .schema('id STRING, timestamp STRING, `MowerApp_Error_errorCode` STRING, `pos-x` INT, `pos-y` INT')\
+        .format("csv")\
+        .option("header", "true")\
+        .load(stream_directory)
+    
+    
+    streamingDF.printSchema()
+
+    df = streamingDF
+
+    mapping = spark.read.format("parquet").load('data/mappings/error_codes')
+    dict = mapping.toPandas().to_dict()
+
+        
+    df.show()
+
+    states = []
+    for row in df.collect():
+            states.append(dict[row['MowerApp_Error_errorCode']][0])
+
+    print(states)
+    pdf = df.toPandas()
+    pdf.insert(5, "state", states)
+
+    df = spark.createDataFrame(pdf)
+    df.show()
+
+    finalDF = df
+
+
+    # Start the streaming query
+    query = finalDF \
+        .writeStream \
+        .format("console") \
+        .foreachBatch(handle_stream_prediction) \
+        .option("truncate", False) \
+        .start()
+
+    query.awaitTermination()
---
+++ applications/train_mower_error_codes.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+import pandas as pd
+from pyspark.sql.functions import col, expr, when
+
+spark = SparkSession.builder\
+        .appName("Mower error codes")\
+        .master("spark://spark-master:7077")\
+        .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+input_df = spark.read.options(inferSchema='True').csv("data/mower/error_codes/train.csv", header=True)
+
+input_df.show()
+dict = {row['MowerApp_Error_errorCode']:row['state'] for row in input_df.collect()}
+print(dict)
+pdf = pd.DataFrame(dict, index=[0])
+df = spark.createDataFrame(pdf)
+df.show()
+df.write.format("parquet").mode("overwrite").save('data/mappings/error_codes')
+
+mapping = spark.read.format("parquet").load('data/mappings/error_codes')
+dict = mapping.toPandas().to_dict()
+
+    
+test_df = input_df.select('id','timestamp','MowerApp_Error_errorCode','pos-x','pos-y')
+test_df.show()
+
+states = []
+for row in test_df.collect():
+        states.append(dict[row['MowerApp_Error_errorCode']][0])
+
+print(states)
+pdf = test_df.toPandas()
+pdf.insert(5, "state", states)
+
+print(pdf)
+df = spark.createDataFrame(pdf)
+df.show()
